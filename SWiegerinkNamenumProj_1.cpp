469e9b32f97f2171ae0ae86648355ef198e611db
shuler wiegerink <stellarpie225@gmail.com>
2025-03-12 11:54:42 -0400

    Create SWiegerinkNamenumProj_1.cpp

---
+++ SWiegerinkNamenumProj_1.cpp
@@ -0,0 +1,23 @@
+/*
+ID: stwiege1
+TASK: namenum
+LANG: C++
+*/
+
+using namespace std;
+#include<fstream>
+#include<string>
+#include<vector>
+
+int main()
+{
+    ifstream fin ("namenum.in");
+    ofstream fout ("namenum.out");
+    ifstream fin2 ("dict.txt");
+
+    int inputNumb, stupidVar;
+    fin >> inputNumb;
+    fin2 >> stupidVar;
+
+    fout << inputNumb << stupidvar << endl;
+}
