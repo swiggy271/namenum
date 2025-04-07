97da3abc3a151be7a973b4ac24c65f289e84c965
shuler wiegerink <stellarpie225@gmail.com>
2025-04-07 11:28:27 -0400

    Update and rename SWiegerinkNamenumProj_11.cpp to SWiegerinkNamenumProj_12.cpp

--- SWiegerinkNamenumProj_12.cpp
+++ SWiegerinkNamenumProj_12.cpp
@@ -24,7 +24,7 @@ int main()
     // initialize and declare variables
     int inputNumb;
     fin >> inputNumb;
-    char TTT[11][3] = {{}, {}, {'A', 'B', 'C'}, {'D', 'E', 'F'}, {'G', 'H', 'I'}, {'J', 'K', 'L'}, {'M', 'N', 'O'}, {'P', 'R', 'S'}, {'T', 'U', 'V'}, {'W', 'X', 'Y'}};
+    char TTT[11][3] = {{''}, {''}, {'A', 'B', 'C'}, {'D', 'E', 'F'}, {'G', 'H', 'I'}, {'J', 'K', 'L'}, {'M', 'N', 'O'}, {'P', 'R', 'S'}, {'T', 'U', 'V'}, {'W', 'X', 'Y'}};
     vector <char> word(inputNumb);
     for(int j = 0; pow(to_string(inputNumb).length(), 3); j++)
     {
