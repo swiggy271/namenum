58a0567337338e5ad6e0102dfa57de095b539549
shuler wiegerink <stellarpie225@gmail.com>
2025-04-23 18:51:47 -0400

    Update and rename SWiegerinkNamenumProj_22.cpp to SWiegerinkNamenumProj_23.cpp

--- SWiegerinkNamenumProj_23.cpp
+++ SWiegerinkNamenumProj_23.cpp
@@ -29,7 +29,7 @@ int main()
         for(int q = 0; q < to_string(inputNumb).length(); q++)
         {
             // word[q] = TTT[int(to_string(inputNumb)[q]) - '0'][j % int(pow(q, 3))];
-            word[q] = TTT[int(to_string(inputNumb)[q]) - '0'][(j % int(pow(q, 3))) % 3];
+            word[q] = TTT[int(to_string(inputNumb)[q]) - '0'][(j % int(pow(3, q))) % 3];
             cout << word[q];
         }
         cout << endl;
